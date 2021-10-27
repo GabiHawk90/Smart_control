02efd99a05e3237deb12663e2745f8671a8d8cea
GabiHawk90 <80804518+GabiHawk90@users.noreply.github.com>
2021-10-27 17:02:46 -0600

    finished temp, lights and second button

--- mid_Term_screen/mid_Term_screen.ino
+++ mid_Term_screen/mid_Term_screen.ino
@@ -6,33 +6,30 @@
 #include <Ethernet.h>
 #include <mac.h>
 #include <hue.h>
-
+#include <Adafruit_BME280.h>
 #define SCREEN_WIDTH 128 //OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED dispaly width, in pixels
-
-// Declaration for an SSD1306 display connected to I2C (SDA,SCL pins)
-// The pins for I2C are defined by the Wire-library.
-// On an arduino UNO:   A4(SDA), A5(SCL)
-// On an arduino MEGA 2560: 20(SDA), 21(SCL)
-// On an arduino LEONARDO: 2(SDA), 3 (SCL), ...
 #define OLED_RESET  76// Rest pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0X3C ///< See datasheet for address; 0x3D for 128x64,
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-//#define
-
 #define LOGO_HEIGHT 16
 #define LOGO_WIDTH  16
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+Adafruit_BME280 bme;
+
+bool BmeStatus;
 bool buttonState;
 bool buttonStateTwo;
 bool isStarted;
 const int ButtonPIN = 23;
 const int ButtonPinTwo = 22;
+const byte BmeAddress = 0x76;
+const char degree = 0xF8 ; // Decimal 248 = 0 xF8
+float tempC;
+float tempF;
+int status;
 
 OneButton button1 (ButtonPIN , false);
 OneButton buttonTwo (ButtonPinTwo, false);
-
-
 
 void setup() {
   Serial.begin(9600);
@@ -41,46 +38,28 @@ void setup() {
   button1.setPressTicks(4000);
   buttonTwo.attachClick(clickTwo);
 
-//  pinMode (A0, INPUT);
-//  randomSeed (analogRead(A0));
-
-
-
-
-  // SSD1306_SWITCHAPVCC = generate display voltage from 3.3V Internally
   if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
     for (;;); //Dont proceed, loop forever
+  }
+
+  BmeStatus = bme.begin (BmeAddress);
+  if (BmeStatus == false) {
+    Serial.printf("BME280 at address 0x%02 X failed tostart", BmeAddress);
   }
 
   pinMode(10, OUTPUT);
   digitalWrite(10, HIGH);
   pinMode(4, OUTPUT);
   digitalWrite(4, HIGH);
-
   Ethernet.begin(mac);
-  delay(200);          //ensure Serial Monitor is up and running
+  delay(200);
   printIP();
   Serial.printf("LinkStatus: %i  \n", Ethernet.linkStatus());
-  //Show initial display buffer contents on the screen --
-  //the library initializes this with an Adafruit splash screen.
   display.display();
-  delay(2000); //Pause for 2 seconds
+  delay(2000);
 
-  //Clear the buffer
   display.clearDisplay();
-
-  //Draw a single pixel in white
-  //  display.drawPixel(10, 10, SSD1306_WHITE);
-
-  //Show the display buffer on the screen. You MUST call display() after
-  //drawing commands to make them visible on screen!
-  //  display.display();
-  //  delay(2000);
-  // display.display() is NOT necessary after every single drawing command,
-  // unless that's what you want...rather, you can batch up a bunch of
-  // drawing operations and then update the screen all at once by calling
-  // display.display(). These examples demonstrate both approaches...
 
   testfillcircle();
 
@@ -92,11 +71,14 @@ void setup() {
 
 void loop() {
 
+  tempC = bme.readTemperature();
+  tempF = (tempC * 9.0 / 5.0) + 32;
+  testtempdegree();
 
   button1.tick();
   buttonTwo.tick();
   if (buttonState) {
-    testdrawstyles();
+    testfamilycall();
     buttonState = false;
     int randoCool = random(40000, 60000);
 
@@ -107,7 +89,7 @@ void loop() {
   }
 
   if (buttonStateTwo) {
-    testdrawstyles();
+    testemergstyles();
     buttonStateTwo = false;
     int randoWarm = random(100, 10000);
 
@@ -116,6 +98,7 @@ void loop() {
       delay (100);
     }
   }
+
   if (isStarted) {
     setHue(5, false, 0, 0, 0);
     delay(1000);
@@ -123,71 +106,66 @@ void loop() {
   }
 }
 
-
-
-void testdrawstyles(void) {
-  display.clearDisplay();
-  display.setTextSize(5);
-  display.setTextColor(SSD1306_WHITE);
-  display.setRotation(0);
-  display.setCursor(7, 7);
-  display.printf("Help", 47, 47);
-  display.display();
-  delay(1000);
+void testtempdegree(void) {
 
   display.clearDisplay();
-  display.setTextSize(5);
+  display.setTextSize(4);
   display.setTextColor(SSD1306_WHITE);
   display.setRotation(0);
-  display.setCursor(35, 10);
-  display.printf("is", 47, 47);
+  display.setCursor(7, 9);
+  display.printf("%0.1f%c", tempF, degree);
   display.display();
-  delay(1000);
-
-  display.clearDisplay();
-  display.setTextSize(5);
-  display.setTextColor(SSD1306_WHITE);
-  display.setRotation(0);
-  display.setCursor(35, 10);
-  display.printf("on", 47, 47);
-  display.display();
-  delay(1000);
-
-  display.clearDisplay();
-  display.setTextSize(5);
-  display.setTextColor(SSD1306_WHITE);
-  display.setRotation(0);
-  display.setCursor(20, 10);
-  display.printf("the", 47, 47);
-  display.display();
-  delay(1000);
-
-
-  display.clearDisplay();
-  display.setTextSize(5);
-  display.setTextColor(SSD1306_WHITE);
-  display.setRotation(0);
-  display.setCursor(20, 10);
-  display.printf("way", 47, 47);
-  display.display();
-  delay(1000);
 }
 
-//void testscrolltext(void) {
-//  display.clearDisplay();
-//
-//  display.setTextSize(2); // Draw 2X-scale text
-//  display.setTextColor(SSD1306_WHITE);
-//  display.setCursor(20, 10);
-//  display.println("Help is on the way.");
-//  display.display();      // Show initial text
-//  delay(100);
+void testemergstyles(void) {
+  display.clearDisplay();
+  display.setTextSize(3);
+  display.setTextColor(SSD1306_WHITE);
+  display.setRotation(0);
+  display.setCursor(10, 7);
+  display.printf("Dialed", 47, 47);
+  display.display();
+  delay(800);
 
-// Scroll in various directions, pausing in-between:
-//display.startscrollleft(0x1, 0x07);
-//  delay(2000);
-//display.stopscroll();
-//delay(1000);
+  display.clearDisplay();
+  display.setTextSize(3);
+  display.setTextColor(SSD1306_WHITE);
+  display.setRotation(0);
+  display.setCursor(35, 10);
+  display.printf("911", 47, 47);
+  display.display();
+  delay(800);
+}
+
+void testfamilycall(void) {
+  display.clearDisplay();
+  display.setTextSize(3);
+  display.setTextColor(SSD1306_WHITE);
+  display.setRotation(0);
+  display.setCursor(10, 7);
+  display.printf("Dialed", 47, 47);
+  display.display();
+  delay(800);
+
+  display.clearDisplay();
+  display.setTextSize(3);
+  display.setTextColor(SSD1306_WHITE);
+  display.setRotation(0);
+  display.setCursor(10, 7);
+  display.printf("Family", 47, 47);
+  display.display();
+  delay(800);
+}
+
+//  display.clearDisplay();
+//  display.setTextSize(5);
+//  display.setTextColor(SSD1306_WHITE);
+//  display.setRotation(0);
+//  display.setCursor(20, 10);
+//  display.printf("way", 47, 47);
+//  display.display();
+//  delay(800);
+//}
 
 void testfillcircle(void) {
   display.clearDisplay();
